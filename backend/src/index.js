67c004c0479b872bc9d36d065268a9c1a6e3e18d
Deepak Sharma <deepak.sh798@gmail.com>
2025-11-25 21:16:26 +0530

    folder and structure

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 5000");
+  connectDB();
+});
