ac2e8229948766d1b3f2c5ee8efdf4e0e94f6f43
Deepak Sharma <deepak.sh798@gmail.com>
2025-12-02 23:49:10 +0530

    add Sidebar

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -7,6 +7,7 @@ export const useAuthStore = create((set: any) => ({
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
+  onlineUsers: [],
 
   isCheckingAuth: true,
 
---
+++ frontend/src/store/useChatStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set: any) => ({
+  message: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId: string) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ message: res.data });
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+
+  // todo: optimize later
+  setSelectedUser: (user: any) => {
+    set({ selectedUser: user });
+  },
+}));
