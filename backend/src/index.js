d484b208f2d653b4350276ab37409613430bae7e
Deepak Sharma <deepak.sh798@gmail.com>
2025-12-12 11:29:26 +0530

    update socket.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,13 @@
-import express from "express";
+// index.js
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: [
-      "https://real-time-chat-application.deepak-sh798.workers.dev",
-      "http://localhost:5173",
-    ],
-    credentials: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log("Server is running on port 5000");
+  console.log(`Server running on port ${PORT}`);
   connectDB();
 });
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,11 +1,43 @@
-import { Server } from "socket.io";
-import http from "http";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "../routes/auth.route.js";
+import messageRoutes from "../routes/message.route.js";
 
 const app = express();
-app.set("trust proxy", 1); //
-const server = http.createServer(app); // here we create an HTTP server, which is needed for Socket.IO
 
+// IMPORTANT for Render (reverse proxy)
+app.set("trust proxy", 1);
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [
+      "https://real-time-chat-application.deepak-sh798.workers.dev",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
+
+// Simple test route
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// API routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// HTTP server for Express + Socket.io
+const server = http.createServer(app);
+
+// Socket setup
 const io = new Server(server, {
   cors: {
     origin: [
